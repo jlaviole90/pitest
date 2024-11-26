d115b58974578875fe9d9821e08d6043f1e1841f
joshualaviolette <jlaviolette@qualbe.com>
2024-11-26 15:45:33 -0600

    test again

--- main.go
+++ main.go
@@ -6,23 +6,31 @@ import (
 )
 
 func main() {
-    err := rpio.Open()
-    if err != nil {
-        panic(err)
-    }
+    _ = rpio.Open()
 
     pinNine := rpio.Pin(0)
     pinTen := rpio.Pin(10)
 
     pinNine.Input()
     pinTen.Input()
+    count := 0
     for {
+        println(pinNine.Read())
+        time.Sleep(100 * time.Millisecond)
+        count++
+
+        if count >= 10 {
+            rpio.Close()
+            _ = rpio.Open()
+        }
+        /*
         if pinNine.Read() == rpio.High {
             break;
         } else {
             println(pinTen.Read())
             time.Sleep(100 * time.Millisecond)
         }
+        */
     }
 
     rpio.Close()
